f6816a288791451630079ee8ced163954360e6d3
eeue56 <enalicho@gmail.com>
2018-01-15 18:50:37 +0100

    add elm-make compleitions

---
+++ _elm_make.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+packages=()
+
+_suggest_elm_make_flags()
+{
+    flag_options="--yes --help --output --report --debug --warn --docs"
+    cur=$1
+    COMPREPLY=( $(compgen -W "${flag_options}" -- $cur) )
+}
+
+_elm_make() 
+{
+    local cur prev contains is_install
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    _contains_root_arg cur
+    contains=$?
+
+    if [[ $cur == -* ]]; then
+        _suggest_elm_make_flags $cur
+        return 0
+    fi
+}
+complete -F _elm_make "elm-make"
